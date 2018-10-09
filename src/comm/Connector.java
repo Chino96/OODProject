b0f0a6056bcd836ac355ca3094083a78143f763a
Chino-PC <Chino-PC@10.40.7.55>
2018-10-09 16:34:02 -0400

    commit1

---
+++ src/comm/Connector.java
@@ -0,0 +1,85 @@
+package comm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connector {
+
+	private Connection connect;
+	private Statement statment;
+	private ResultSet resultSet;
+	
+	private final String JDBCCON = "jdbc:postgresql:";
+	private final String URL = "//georgiasouthern.chqyded5qfex.us-east-1.rds.amazonaws.com:";
+	private String port;
+	private String dbName;
+	private String user;
+	private String password;
+
+	public Connector() {
+		port = "5432";
+		dbName = "/harrisgs";
+		user = "harris";
+		password = "professorgs";
+	}
+
+	public Connector(String usr, String pass) {
+		port = "5432";
+		dbName = "/harrisgs";
+		user = usr;
+		password = pass;
+	}
+
+	public Connector(String por, String db, String usr, String pass) {
+		this.port = por;
+		this.dbName = db;
+		this.user = usr;
+		this.password = pass;
+	}
+
+	public int connect() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Connecting to Database......");
+			connect = DriverManager.getConnection(JDBCCON + URL + this.port + this.dbName, 
+					this.user,
+					this.password);
+			System.out.println("Connection Successful");
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			return 1;
+		}
+
+		return 0;
+	}
+	
+	public ResultSet MakeQuery(String s) {
+		try {
+			this.resultSet = this.getStatment().executeQuery(s);
+		} catch (SQLException e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+		return this.resultSet;
+	}
+	
+	public void update(String s) {
+		try {
+			this.getStatment().executeUpdate(s);
+		}
+		catch(SQLException e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+	}
+	
+	public Statement getStatment() {
+		try {
+			this.statment = connect.createStatement();
+		} catch (SQLException e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+		return statment;
+	}
+}
---
+++ src/comm/DataBase.java
@@ -0,0 +1,27 @@
+package comm;
+
+import java.sql.ResultSet;
+
+public class DataBase {
+
+	private Connector con;
+
+	public DataBase() {
+		con = new Connector();
+	}
+	
+	public void Write(String s) {
+		this.con.connect();
+		this.con.update(s);
+	}
+	
+	public ResultSet Read(String s) {
+		if(!s.contains("SELECT")) {
+			return null;
+		}
+		this.con.connect();
+		ResultSet set = this.con.MakeQuery(s);
+		return set;
+	}
+
+}
