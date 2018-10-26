4d110223e5f4f56e1ee27e15190b20804993c7e6
as12660 <as12660@georgiasouthern.edu>
2018-10-26 00:05:18 -0400

    Ready to Connect

--- src/gui/gradeReport.java
+++ src/gui/gradeReport.java
@@ -4,26 +4,54 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import comm.Connector;
 
 public class gradeReport {
 
+	//delete main method once connected for real
 	public static void main(String[] args) {
 		String fileName = "testquiz";
 		createGradeReportTxt(fileName);
-		appendGradeReportTxt(fileName);
-
+		try {
+			appendGradeReportTxt(fileName);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	//Pulls in Information for Grade Report
-	public static void appendGradeReportTxt(String fileName) {
+	public static void appendGradeReportTxt(String fileName) throws SQLException {
 		String userHomePath = System.getProperty("user.home");
 		String gradeReport = userHomePath+"\\Documents\\Reports\\Scores\\"+fileName+".scores.txt";
 		
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(gradeReport))) {
 
-			String content = "\t\t\t\t\t"+fileName+" Scores";
-
-			bw.write(content);
+			bw.write("\t\t\t\t\t"+fileName+" Scores\n");
+			
+			Connector con = new Connector();
+			con.connect();
+			//select from statement gets emails and grade
+			ResultSet resultSetAvg = con.MakeQuery("Select AVG(\"finalGrade\") from \""+fileName+"\"");
+			
+			//Calc the average for the class
+			while (resultSetAvg.next()) { 
+				Double average = resultSetAvg.getDouble(1);
+				String saverage = Double.toString(Math.round(average * 100.0)/100.0);
+				bw.write("The Class Average is "+saverage+"\n\n");
+			}
+			
+			ResultSet resultSet = con.MakeQuery("Select \"studentEmail\",\"finalGrade\" from \""+fileName+"\"");	
+			while (resultSet.next()) {    
+				String email = resultSet.getString(1);
+				Double grade = resultSet.getDouble(2);
+				String sgrade = Double.toString(Math.round(grade * 100.0)/100.0);
+				bw.write(email + " -\t "); //Print email
+				bw.write(sgrade); //Print grade
+				bw.write("\n");//Move to the next line to print the next row.           
+			}
 
 			System.out.println("Done");
 
